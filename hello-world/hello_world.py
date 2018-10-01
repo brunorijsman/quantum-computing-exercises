7d47f0c148b1f3896a9460a3df82090a83840d70
Bruno Rijsman <brunorijsman@gmail.com>
2018-10-01 11:34:00 -0700

    Be consistent

--- hello-world/hello_world.py
+++ hello-world/hello_world.py
@@ -4,9 +4,9 @@ import qiskit
 if "QX_API_TOKEN" not in os.environ:
     print("Set environment variable QX_API_TOKEN")
     exit(1)
-API_TOKEN = os.environ["QX_API_TOKEN"]
+QX_API_TOKEN = os.environ["QX_API_TOKEN"]
 
-qiskit.register(API_TOKEN, "https://quantumexperience.ng.bluemix.net/api")
+qiskit.register(QX_API_TOKEN, "https://quantumexperience.ng.bluemix.net/api")
 print("Registered")
 
 print("Available backends:")
